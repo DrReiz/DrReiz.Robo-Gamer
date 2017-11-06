72225e7fe7ceb266e647de15b06354add80bce40
DrReiz <dr.anreiz@gmail.com>
2017-11-06 22:34:10 +0300

    Gumball gamer added

---
+++ DrReiz.GumballGamer/Program.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrReiz.GumballGamer
+{
+    class Program
+    {
+        //C:\Users\Serge\AppData\Local\Android\sdk\platform-tools >  adb start-server
+        //adb devices -- посмотреть список устройств
+        //adb kill-server -- с последующим start-server, помогает увидеть blue-stacks
+        //
+        //посмотреть текущее разрешение
+        //adb shell dumpsys displays
+        //https://stackoverflow.com/questions/7527459/android-device-screen-size
+        static void Main(string[] args)
+        {
+            var vm_title = "BlueStacks";
+
+            var handle = DirectGamer.FindWindow(null, vm_title);
+            if (handle == IntPtr.Zero)
+                throw new Exception("Окно не найдено");
+
+            //DirectGamer.PostMessage(new System.Runtime.InteropServices.HandleRef(null, handle), DirectGamer.WM_SIZE, new IntPtr(0), new IntPtr((1200 << 16) + 600));
+
+            DirectGamer.RECT rect;
+            DirectGamer.GetWindowRect(handle, out rect);
+            Console.WriteLine($"{rect.Right - rect.Left} {rect.Bottom - rect.Top}");
+            var screenshot = DirectGamer.GetScreenImage(new System.Drawing.Rectangle(rect.Left, rect.Top, rect.Right - rect.Left, rect.Bottom - rect.Top));
+            screenshot.Save("gumballs.screenshot.png");
+
+            var screenWidth = 900;
+            var screenHeight = 1600;
+            var headerHeight = 40;
+            var footerHeight = 40;
+
+            var cellWidth = 167;
+            //var cellHeight = 180;
+            var cellHeight = 156;
+            var mazeX = 30;
+            var mazeY = 277;
+
+            if (true)
+            {
+                var dx = screenshot.Width / (float)screenWidth;
+                var dy = (screenshot.Height - headerHeight - footerHeight) / (float)screenHeight;
+
+                var image = new Bitmap(screenshot);
+                using (var g = Graphics.FromImage(image))
+                {
+                    var pen = new Pen(Color.Red);
+
+                    Action<Pen, float, float, float, float> drawLine = (p, x1, y1, x2, y2) => g.DrawLine(p, x1 * dx, y1 * dy + headerHeight, x2 * dx, y2 * dy + headerHeight);
+
+                    for (var y = 0; y <= 6; ++y)
+                        drawLine(pen, mazeX, mazeY + y * cellHeight, mazeX + cellWidth * 5, mazeY + y * cellHeight);
+                    for (var x = 0; x <= 5; ++x)
+                        drawLine(pen, mazeX + x * cellWidth, mazeY, mazeX + x * cellWidth, mazeY + 6 * cellHeight);
+                }
+                image.Save("gumballs.lines.png");
+            }
+
+
+            return;
+
+            //DirectGamer.PostMessage(new System.Runtime.InteropServices.HandleRef(null, handle), DirectGamer.WM_LBUTTONDOWN, new IntPtr(DirectGamer.MK_LBUTTON), new IntPtr((100 << 16) + 100));
+            if (true)
+            {
+                // 2 4 ?? дверь, не сработала
+                var x = 0;
+                var y = 4;
+                Shell($"input tap {mazeX + cellWidth / 2 + x * cellWidth} {mazeY + cellHeight/2 + y * cellHeight}"); //maze cell 1 2
+            }
+            //screenshot: cell 1 2: 110 322 - 206 426
+            //Shell("input tap 193 620"); //maze cell 0 2
+
+            //Shell($"input tap 365 {787 + cellHeight}"); //maze cell 1 2
+            //Shell("input tap 193 599"); //maze cell 2 2
+
+            //Shell("input tap 194 600"); //maze cell 1 2
+            //Shell("input tap 364 786"); //maze cell 1 2
+
+            //Shell("input tap 365 787"); //maze cell 2 3
+            return;
+            //Shell($"input tap 10 800"); //maze рамка
+            //Shell($"input tap 20 850"); //maze рамка
+            //Shell($"input tap 25 850"); //maze cell 0 3
+            //Shell("input tap 20 50"); //левый верхний угол
+            //Shell("input tap 100 300"); //maze cell 0 0
+            //Shell($"input tap 170 620"); //maze cell 0 2
+            //Shell($"input tap 300 800"); //maze cell 1 3
+            //Shell("input tap 500 300"); //maze cell 2 0
+            //Shell("input tap 700 300"); //maze cell 3 0
+            //Shell("input tap 650 300"); //maze cell 3 0
+            //Shell("input tap 600 300"); //maze cell 3 0
+
+            //Shell("input tap 750 300"); //maze cell 4 0
+            Shell("input tap 800 300"); //maze cell 4 0
+            //Shell("input tap 900 300"); //вне экрана
+
+            //Shell("input tap 800 300"); //maze cell 3 0
+            //Shell("input tap 00 300"); //maze cell 3 0
+            return;
+            return;
+            Shell("input keyevent 4");//back кнопка
+            Shell("input keyevent 4");
+        }
+        static void Shell(string command)
+        {
+            var port = 5037;
+            //var port = 5555;
+            using (var client = new TcpClient("127.0.0.1", port))
+            {
+                //client.Connect()
+                using (var stream = client.GetStream())
+                {
+                    //SendToAdb(stream, "127.0.0.1:5556:get-product");
+                    //SendToAdb(stream, "host:transport:127.0.0.1:5555", isPayload: false);
+                    SendToAdb(stream, "host:transport:emulator-5554", isPayload: false);
+                    //SendToAdb(stream, "shell:");
+                    //SendToAdbShell(stream, "input keyevent 4\r");
+                    //SendToAdbShell(stream, "input keyevent 4\r");
+                    //System.Threading.Thread.Sleep(TimeSpan.FromSeconds(0.1));
+                    SendToAdb(stream, $"shell:{command}", isPayload: true);
+                    //SendToAdb(stream, "shell:input keyevent 4", isPayload: true);
+                    return;
+                    SendToAdb(stream, "host:devices");
+                    SendToAdb(stream, "host:version");
+                    //SendToAdb(stream, "host:devices");
+
+                    return;
+                    //var command = "input keyevent 4\n";
+                    //var buf = Encoding.UTF8.GetBytes(command);
+                    //stream.Write(buf, 0, buf.Length);
+                }
+            }
+        }
+        static string SendToAdb(NetworkStream stream, string command, bool? isPayload = null)
+        {
+            var fullCommand = $"{command.Length:x4}{command}";
+            Console.WriteLine(fullCommand);
+            var buf = Encoding.UTF8.GetBytes(fullCommand);
+            stream.Write(buf, 0, buf.Length);
+            var okay = ReceiveFromAdb(stream, length: 4);
+            Console.WriteLine($">>{okay.Length} {okay}");
+            if (okay.Substring(0, 4) != "OKAY")
+                throw new Exception($"error adb receive: {okay}");
+            if (isPayload == false)
+                return null;
+            if (isPayload == true)
+            {
+                var payload = ReceiveFromAdbPacket(stream);
+                Console.WriteLine(payload);
+                return payload;
+            }
+            var answer = ReceiveFromAdb(stream);
+            Console.WriteLine(answer);
+            return answer;
+        }
+        static string SendToAdbShell(NetworkStream stream, string command)
+        {
+            Console.WriteLine($">> {command}");
+            var buf = Encoding.UTF8.GetBytes(command);
+            stream.Write(buf, 0, buf.Length);
+            var res = ReceiveFromAdb(stream);
+            Console.WriteLine($"<< {res}");
+            return res;
+        }
+
+        static string ReceiveFromAdb(NetworkStream stream, int? length = null)
+        {
+            var answerBuf = new byte[length ?? 1000];
+            int len = stream.Read(answerBuf, 0, answerBuf.Length);
+            return Encoding.UTF8.GetString(answerBuf, 0, len);
+        }
+        static string ReceiveFromAdbPacket(NetworkStream stream)
+        {
+            var prefix = ReceiveFromAdb(stream, 4);
+            if (prefix.Length == 0)
+                return null;
+            Console.WriteLine($">>{prefix.Length} {prefix}");
+            var len = Convert.ToInt32(prefix);
+            return ReceiveFromAdb(stream, len);
+        }
+    }
+}
