c3248ddaa2ff2d9463c799c8f29e8754d584848f
DrReiz <dr.anreiz@gmail.com>
2018-04-05 04:59:14 +0300

    Ai-Gamer.Wui: cells maked and noted

--- DrReiz.GumballGamer.Wui/ClientApp/components/MazeView.tsx
+++ DrReiz.GumballGamer.Wui/ClientApp/components/MazeView.tsx
@@ -3,39 +3,53 @@ import { RouteComponentProps } from 'react-router';
 
 interface MazeState {
     currentCount: number;
+    is_grid_display: boolean;
+    img_name: string;
 }
 
 export class MazeView extends React.Component<RouteComponentProps<{}>, MazeState> {
     constructor() {
         super();
-        this.state = { currentCount: 0 };
+        this.state =
+          {
+            currentCount: 0, is_grid_display: true,
+            //img_name: "171213.233335.png",
+            img_name: "171213.233408.png",
+          };
     }
 
     public render() {
         let resizeK = 0.5;
         let maze = MazeView.Resize(MazeView.Maze, resizeK);
         let grid = MazeView.ResizeRects(MazeView.Grid(MazeView.Maze.frame.width, MazeView.Maze.frame.height, 200), resizeK);
+        //console.log(maze);
 
         return <div>
             <h1>Maze</h1>
+            <div onClick={() => { this.toggleGridDisplay() }}>grid</div>
             <div style={{ display: 'table-row' }}>
-                <div style={{ width: maze.frame.width, height: maze.frame.height, border: '0px solid black', display: 'table-cell', position: 'relative' }}>
-                    <img style={{ width: maze.frame.width, height: maze.frame.height, position: 'absolute' }} src='data/171213.233335.png' / >
-                    {grid.map(area =>
-                        <div style={{ left: area.x, top: area.y, width: area.width, height: area.height, position: 'absolute', border:'1px solid lightblue' }}>
+                <div style={{ width: maze.frame.width, height: maze.frame.height, border: '0px solid black', display: 'table-cell', position: 'relative', color:'lightblue' }}>
+                    <img style={{ width: maze.frame.width, height: maze.frame.height, position: 'absolute' }} src={'data/' + this.state.img_name} / >
+                    {
+                      this.state.is_grid_display ? 
+                      grid.map((area,k) =>
+                        <div style={{ left: area.x, top: area.y, width: area.width, height: area.height, position: 'absolute', border:'1px solid lightblue' }} key={k}>
                         </div>
                        )
+                       :null
                     }
-                    {maze.areas.map(area =>
-                        <div style={{ left: area.x, top: area.y, width: area.width, height: area.height, position: 'absolute', border:'1px solid red' }}>
+                    {maze.areas.map((area, k) =>
+                        <div className="maze-cell" style={{ left: area.x, top: area.y, width: area.width, height: area.height, position: 'absolute', border: (MazeView.IsFloat(area.name) ? '2px solid red' : '0.5px solid lightpink'), zIndex: MazeView.IsFloat(area.name)? 10 : 0 }} key={k}>
+                            <div className="maze-text">{area.name}</div>
                         </div>
                        )
                     }
                 </div>
                 <div style={{ width: '20px', display: 'table-cell' }}></div>
-                <div style={{ width: maze.frame.width, height: maze.frame.height, border: '1px solid black', display: 'table-cell', position:'relative' }}>
-                    {maze.areas.map(area =>
-                        <div style={{ left: area.x, top: area.y, width: area.width, height: area.height, position: 'absolute', border:'1px solid black' }}>
+                <div style={{ width: maze.frame.width, height: maze.frame.height, border: '1px solid black', display: 'table-cell', position:'relative', color:'green' }}>
+                    {maze.areas.map((area, k) =>
+                        <div className="maze-cell" style={{ left: area.x, top: area.y, width: area.width, height: area.height, position: 'absolute', border: (MazeView.IsFloat(area.name) ? '2px solid black' : '0.5px solid darkgray'), zIndex: MazeView.IsFloat(area.name) ? 10 : 0 }} key={k}>
+                            <div className="maze-text">{area.value}</div>
                         </div>
                        )
                     }
@@ -43,6 +57,9 @@ export class MazeView extends React.Component<RouteComponentProps<{}>, MazeState
             </div>
 
         </div>;
+    }
+    static IsFloat(name: string | undefined): boolean {
+        return name != null && !name.startsWith('cell-');
     }
     static Grid(width: number, height: number, size: number): Rect[] {
         return [...Array(Math.floor((height - 1) / size - 1)).keys()].map(j => { return { x: 0, y: (j + 1) * size, width: width, height: size }; })
@@ -80,6 +97,11 @@ export class MazeView extends React.Component<RouteComponentProps<{}>, MazeState
             };
         })
     }
+    toggleGridDisplay() {
+        this.setState({
+            is_grid_display: !this.state.is_grid_display
+        });
+    }
 
     //incrementCounter() {
     //    this.setState({
@@ -91,12 +113,49 @@ export class MazeView extends React.Component<RouteComponentProps<{}>, MazeState
             x: 0, y: 0, width: 900, height: 1600
         },
         areas: [
-            { x: 30, y: 280, width: 170, height: 156 },
-            { x: 200, y: 436, width: 170, height: 156 },
-            { x: 370, y: 592, width: 170, height: 156 },
-            { x: 540, y: 748, width: 170, height: 156 },
-            { x: 710, y: 904, width: 170, height: 156 },
-            { x: 540, y: 1060, width: 170, height: 156 },
+            //{ x: 30, y: 280, width: 170, height: 156, name:'cell-0-0', value:'pit' },
+            //{ x: 200, y: 436, width: 170, height: 156, name: 'cell-1-1', value: 'empty' },
+            //{ x: 370, y: 592, width: 170, height: 156, name: 'cell-2-2', value: 'empty' },
+            //{ x: 540, y: 592, width: 170, height: 156, name: 'cell-3-2', value: 'key' },
+            //{ x: 540, y: 748, width: 170, height: 156, name: 'cell-3-3', value: 'empty' },
+            //{ x: 710, y: 904, width: 170, height: 156, name: 'cell-4-4', value: 'slab' },
+            //{ x: 540, y: 1060, width: 170, height: 156 },
+
+            { x: 30, y: 280 + 20, width: 170, height: 156, name:'cell-0-0', value:'pit' },
+            { x: 200, y: 280 + 20, width: 170, height: 156, name: 'cell-1-0', value: 'den' },
+            { x: 370, y: 280 + 20, width: 170, height: 156, name: 'cell-2-0', value: 'den' },
+            { x: 540, y: 280 - 30, width: 170, height: 156, name: 'float-3-0', value: 'enemy' },
+            { x: 710, y: 280, width: 170, height: 156, name: 'cell-4-0', value: 'block' },
+
+            { x: 30, y: 436 + 20, width: 170, height: 156, name: 'cell-0-1', value: 'empty' },
+            { x: 200, y: 436 + 20, width: 170, height: 156, name: 'cell-1-1', value: 'empty' },
+            { x: 370, y: 436 + 20, width: 170, height: 156, name: 'cell-2-1', value: 'empty' },
+            { x: 540, y: 436 + 20, width: 170, height: 156, name: 'cell-3-1', value: 'bee-tree' },
+            { x: 710, y: 436, width: 170, height: 156, name: 'cell-4-1', value: 'block' },
+
+            { x: 30, y: 592 + 20, width: 170, height: 156, name: 'cell-0-2', value: 'tree' },
+            { x: 200, y: 592 + 20, width: 170, height: 156, name: 'cell-1-2', value: 'empty' },
+            { x: 370, y: 592 + 20, width: 170, height: 156, name: 'cell-2-2', value: 'empty' },
+            { x: 540, y: 592 - 30, width: 170, height: 156, name: 'float-3-2', value: 'key' },
+            { x: 710, y: 592 - 30, width: 170, height: 156, name: 'float-4-2', value: 'enemy' },
+
+            { x: 30, y: 748 + 20, width: 170, height: 156, name: 'cell-0-3', value: 'door' },
+            { x: 200, y: 748 + 20, width: 170, height: 156, name: 'cell-1-3', value: 'pit' },
+            { x: 370, y: 748 + 20, width: 170, height: 156, name: 'cell-2-3', value: 'empty' },
+            { x: 540, y: 748 + 20, width: 170, height: 156, name: 'cell-3-3', value: 'empty' },
+            { x: 710, y: 748, width: 170, height: 156, name: 'cell-3-3', value: 'block' },
+
+            { x: 30, y: 904 + 20, width: 170, height: 156, name: 'cell-0-4', value: 'empty' },
+            { x: 200, y: 904 + 20, width: 170, height: 156, name: 'cell-1-4', value: 'empty' },
+            { x: 370, y: 904 + 20, width: 170, height: 156, name: 'cell-2-4', value: 'empty' },
+            { x: 540, y: 904 - 30, width: 170, height: 156, name: 'float-3-4', value: 'EP' },
+            { x: 710, y: 904, width: 170, height: 156, name: 'cell-4-4', value: 'slab' },
+
+            { x: 30, y: 1060 + 20, width: 170, height: 156 - 20, name: 'cell-0-4', value: 'empty' },
+            { x: 200, y: 1060 - 30, width: 170, height: 156, name: 'float-1-5', value: 'arrow' },
+            { x: 370, y: 1060 - 30, width: 170, height: 156, name: 'float-2-5', value: 'mana' },
+            { x: 540, y: 1060 + 20, width: 170, height: 156 - 20, name: 'cell-3-5', value: 'empty' },
+            { x: 710, y: 1060, width: 170, height: 156, name: 'cell-4-5', value: 'slab' },
             ]
     };
 }
