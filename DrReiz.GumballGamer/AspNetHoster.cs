364e177c70447fe127d7977225d48e48cc0cf020
DrReiz <dr.anreiz@gmail.com>
2018-08-08 17:32:39 +0300

    asp.net web api 2 service added

---
+++ DrReiz.GumballGamer/AspNetHoster.cs
@@ -0,0 +1,46 @@
+﻿using NitroBolt.CommandLine;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace DrReiz.GumballGamer
+{
+    public class AspNetHoster
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+
+            // Configure Web API for self-host. 
+
+            var config = new HttpConfiguration();
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
+            config.MapHttpAttributeRoutes();
+
+            appBuilder.UseWebApi(config);
+
+            config.EnsureInitialized(); //без этой команды проглатывает ошибки инициализации
+        }
+
+        [CommandLine("web-service")]
+        public static void Execute()
+        {
+            string baseAddress = "http://localhost:8471/";
+
+            using (Microsoft.Owin.Hosting.WebApp.Start<AspNetHoster>(url: baseAddress))
+            {
+                Console.WriteLine("web-service started");
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
---
+++ DrReiz.GumballGamer/MapController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace DrReiz.GumballGamer
+{
+    public class MapController: ApiController
+    {
+        [Route("ping")]
+        [HttpGet]
+        public string Ping()
+        {
+            return "gamer.web-service";
+        }
+    }
+}
