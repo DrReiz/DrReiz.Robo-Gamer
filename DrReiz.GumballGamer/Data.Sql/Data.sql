6adc2e74b0a351c0557716a5fee55f9bbc7b21d0
DrReiz <dr.anreiz@gmail.com>
2018-09-04 16:09:32 +0300

    linq2db added

---
+++ DrReiz.GumballGamer/Data.Sql/Data.sql
@@ -0,0 +1,11 @@
+﻿create table Processing
+(
+  Id uniqueidentifier NOT NULL PRIMARY KEY,
+  Name nvarchar(200) NOT NULL,
+  ProcessedTime datetime2 NOT NULL default('01.01.1900'),
+  ProcessedKeys nvarchar(max) NULL,
+
+  UpdateTime datetime2 NOT NULL default('01.01.1900'),
+
+  --INDEX Processing_Name_Index (Name)
+);
---
+++ DrReiz.GumballGamer/Data/Storage.cs
@@ -0,0 +1,71 @@
+﻿using LinqToDB;
+using LinqToDB.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrReiz.GumballGamer
+{
+    public class GamerDataContext: LinqToDB.Data.DataConnection
+    {
+        public static readonly string DefaultConnectionString = @"Server=localhost;Database=RoboGamer;Trusted_Connection=True;";
+        public GamerDataContext(string connectionString = null)
+            : base(connectionString ?? DefaultConnectionString)
+        {
+        }
+
+        public ITable<Step> Steps => GetTable<Step>();
+        public ITable<Hsv> Hsvs => GetTable<Hsv>();
+        public ITable<Processing> Processings => GetTable<Processing>();
+
+    }
+
+    [Table(Name = "Step")]
+    public class Step
+    {
+        [PrimaryKey]
+        public Guid Id = Guid.NewGuid();
+        [Column]
+        public string Game;
+        [Column]
+        public string Source;
+        [Column]
+        public string Target;
+        [Column]
+        public string Action;
+        [Column]
+        public DateTime Time = DateTime.UtcNow;
+    }
+
+    [Table(Name = "Hsv")]
+    public class Hsv
+    {
+        [PrimaryKey]
+        public string Shot;
+        [Column]
+        public int H;
+        [Column]
+        public int S;
+        [Column]
+        public int V;
+    }
+
+    [Table(Name = "Processing")]
+    public class Processing
+    {
+        [PrimaryKey]
+        public Guid Id;
+        [Column]
+        public string Name;
+        [Column]
+        public DateTime ProcessedTime;
+        [Column]
+        public string Keys;
+
+        [Column]
+        public DateTime UpdateTime;
+    }
+
+}
